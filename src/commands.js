70a8253fd6ad2ece200c223616f55a6569bfc9d6
Florent Georges <fgeorges@fgeorges.org>
2018-04-10 15:04:41 +0200

    Make sure to resolve directory names.

--- src/commands.js
+++ src/commands.js
@@ -40,23 +40,23 @@
 
                 // create `src/`
                 // TODO: Create `test/` as well, when supported.
-                var srcdir = pf.resolve('src', vars.dir);
+                var srcdir = pf.resolve('src/', vars.dir);
                 pf.mkdir(srcdir, force);
 
                 // create `xproject/` and `xproject/project.xml`
-                var xpdir = pf.resolve('xproject', vars.dir);
+                var xpdir = pf.resolve('xproject/', vars.dir);
                 pf.mkdir(xpdir, force);
                 pf.write(pf.resolve('project.xml', xpdir), NEW_PROJECT_XML(vars), force);
 
                 // create `xproject/mlenvs/` and `xproject/mlenvs/{base,default,dev,prod}.json`
-                var mldir = pf.resolve('mlenvs', xpdir);
+                var mldir = pf.resolve('mlenvs/', xpdir);
                 pf.mkdir(mldir, force);
                 pf.write(pf.resolve('base.json',    mldir), NEW_BASE_ENV(vars),    force);
                 pf.write(pf.resolve('default.json', mldir), NEW_DEFAULT_ENV(vars), force);
                 pf.write(pf.resolve('dev.json',     mldir), NEW_DEV_ENV(vars),     force);
                 pf.write(pf.resolve('prod.json',    mldir), NEW_PROD_ENV(vars),    force);
 
-                this.xpdir  = xpdir;
+                this.xpdir = xpdir;
             }, this));
             return actions;
         }
